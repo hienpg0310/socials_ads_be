882c023aa6a5c0297eb4e4c5fef788518adccddd
Gia Hien Pham <hienhine0310@gmail.com>
2025-07-08 14:21:56 +0700

    update

--- prisma/migrations/20250603081951_add_status_social_page_setting/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "SettingStatus" AS ENUM ('connected', 'not_connected');
-
--- AlterTable
-ALTER TABLE "SocialPlatformSetting" ADD COLUMN     "status" "SettingStatus" NOT NULL DEFAULT 'not_connected';
--- prisma/migrations/20250610075558_change_platform_name_in_post/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `platforms` on the `Post` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Post" DROP COLUMN "platforms",
-ADD COLUMN     "platformIds" TEXT[] DEFAULT ARRAY[]::TEXT[];
